011478e75674135965233098a647cda370898806
ajay <ajaysunarthi@iitj.ac.in>
2016-04-05 23:40:23 +0530

    FizzBuzz task done, with correction in controller

--- app/controllers/FizzBuzzController.js
+++ app/controllers/FizzBuzzController.js
@@ -1,7 +1,30 @@
 'use strict';
 
 angular.module('TestDemoApp').controller('FizzBuzzController', function() {
-	var vm = this;
+    var vm = this;
+
     vm.fizzBuzz = function(num) {
+
+        var result = '';
+        for (var i = 1; i <= num; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                result = result + 'FizzBuzz';
+            }
+            else if (i % 3 == 0) {
+                result = result + 'Fizz';
+            }
+
+            else if (i % 5 == 0) {
+                result = result + 'Buzz';
+            } 
+
+            else {
+                result = result + i;
+            }
+        }
+        return result;
     };
+
 });
+
--- test/unit/controllers/FizzBuzzControllerSpec.js
+++ test/unit/controllers/FizzBuzzControllerSpec.js
@@ -13,14 +13,14 @@ describe("FizzBuzzController", function() {
 
     describe("FizzBuzzController.fizzBuzz", function() {
 
-        // it("should have the correct string for 15 integers", function() {
-        //
-        //     expect(controller.fizzBuzz(15)).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz');
-        // });
-        //
-        // it("should have the correct string for 30 integers", function() {
-        //
-        //     expect(controller.fizzBuzz(15)).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz');
-        // });
+         it("should have the correct string for 15 integers", function() {
+        
+             expect(controller.fizzBuzz(15)).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz');
+         });
+        
+         it("should have the correct string for 30 integers", function() {
+        
+             expect(controller.fizzBuzz(30)).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz');
+         });
     });
 });
