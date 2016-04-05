7e9ad7df57b499661963a3c77be145e24172ec36
ajay <ajaysunarthi@iitj.ac.in>
2016-04-05 23:58:49 +0530

    Text search done test is left yet

--- app/controllers/TextSearchController.js
+++ app/controllers/TextSearchController.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('TestDemoApp').controller('TextSearchController', function() {
-  var vm = this;
-  // Count the number of occurrences the text in the box appears.
-  vm.textChange = function() {
-    //   $scope.search;
-    //   $scope.searchText;
-    //   $scope.occurrences;
-  };
+    var vm = this;
+    // Count the number of occurrences the text in the box appears.
+    vm.textChange = function() {
+        //   $scope.search;
+        //   $scope.searchText;
+        var regX = new RegExp(vm.search, 'g');
+        vm.occurrences = (vm.searchText.match(regX) || []).length;
 
-  vm.searchText = "Speakers often repeat the first word of major constituents, as in, “I uh I wouldn't be surprised at that.” Repeats like this divide into four stages: an initial commitment to the constituent (with “I”); the suspension of speech; a hiatus in speaking (filled with “uh”); and a restart of the constituent (“I wouldn't . . .”). An analysis of all repeated articles and pronouns in two large corpora of spontaneous speech shows that the four stages reflect different principles. Speakers are more likely to make a premature commitment, immediately suspending their speech, as both the local constituent and the constituent containing it become more complex. They plan some of these suspensions from the start as preliminary commitments to what they are about to say. And they are more likely to restart a constituent the more their stopping has disrupted its delivery. We argue that the principles governing these stages are general and not specific to repeats.";
+    };
+
+    vm.searchText = "Speakers often repeat the first word of major constituents, as in, “I uh I wouldn't be surprised at that.” Repeats like this divide into four stages: an initial commitment to the constituent (with “I”); the suspension of speech; a hiatus in speaking (filled with “uh”); and a restart of the constituent (“I wouldn't . . .”). An analysis of all repeated articles and pronouns in two large corpora of spontaneous speech shows that the four stages reflect different principles. Speakers are more likely to make a premature commitment, immediately suspending their speech, as both the local constituent and the constituent containing it become more complex. They plan some of these suspensions from the start as preliminary commitments to what they are about to say. And they are more likely to restart a constituent the more their stopping has disrupted its delivery. We argue that the principles governing these stages are general and not specific to repeats.";
 });
